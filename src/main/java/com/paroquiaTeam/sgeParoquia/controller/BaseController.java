126da58c143f876805f397fb1b57ea59ed62114e
sebastiandevus <sebastiandevus@gmail.com>
2026-05-21 15:54:46 -0300

    Adiciona botões de navegação à barra lateral

--- src/main/java/com/paroquiaTeam/sgeParoquia/controller/BaseController.java
+++ src/main/java/com/paroquiaTeam/sgeParoquia/controller/BaseController.java
@@ -5,9 +5,12 @@ import java.io.UncheckedIOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
@@ -15,17 +18,38 @@ public class BaseController {
 	@FXML private AnchorPane contentArea;
 	@FXML private VBox sideBar;
 	
+	// nav buttons
+	@FXML private ToggleButton btnNavConfig;
+	@FXML private ToggleButton btnNavVagas;
+	
 	private final Map<String, Parent> cacheScreens = new HashMap<String, Parent>();
 	
 	private static BaseController instance;
 	
 	@FXML
 	private void initialize() {
+		ToggleGroup grupoNav = new ToggleGroup();
+		btnNavConfig.setToggleGroup(grupoNav);
+		btnNavVagas.setToggleGroup(grupoNav);
+				
+		btnNavConfig.setOnAction(event -> navegar(event, "configuracaoSistema", "/screens/configuracaoSistema/configuracaoSistema.fxml"));
+		btnNavVagas.setOnAction(event -> navegar(event, "listaVagas", "/screens/listaVagas/listaVagas.fxml"));
+
 		instance = this;
 	}
 	
 	public static BaseController getInstance() {
 		return instance;
+	}
+	
+	private void navegar(ActionEvent e, String id, String caminho) {
+		ToggleButton btn = (ToggleButton) e.getSource();
+		if (!btn.isSelected()) {
+			e.consume();
+			btn.setSelected(true);
+			return;
+		}
+		mostrarTela(id, caminho);
 	}
 	
 	public void mostrarTela(String id, String caminho) {
