aae8c8b003fff42c1a6bc26b9181a4fe0734c3b1
sebastiandevus <sebastiandevus@gmail.com>
2026-05-20 23:41:42 -0300

    Remove Enum TipoPrecificaçãoViewModel. Remove imports não utilizados do controller ConfiguracaoSistemaController

--- src/main/java/com/paroquiaTeam/sgeParoquia/controller/ConfiguracaoSistemaController.java
+++ src/main/java/com/paroquiaTeam/sgeParoquia/controller/ConfiguracaoSistemaController.java
@@ -3,14 +3,8 @@ package com.paroquiaTeam.sgeParoquia.controller;
 import java.util.EnumMap;
 import java.util.Map;
 
-import com.paroquiaTeam.sgeParoquia.dao.EstacionamentoDAO;
-import com.paroquiaTeam.sgeParoquia.dao.PrecificacaoFracionadaDAO;
-import com.paroquiaTeam.sgeParoquia.dao.PrecificacaoPorHoraDAO;
-import com.paroquiaTeam.sgeParoquia.model.PrecificacaoFracionada;
-import com.paroquiaTeam.sgeParoquia.model.PrecificacaoPorHora;
 import com.paroquiaTeam.sgeParoquia.model.TipoPrecificacao;
 import com.paroquiaTeam.sgeParoquia.view.PainelPrecificacao;
-import com.paroquiaTeam.sgeParoquia.view.TipoPrecificacaoViewModel;
 import com.paroquiaTeam.sgeParoquia.view.factory.PainelPrecificacaoFracionadaFactory;
 import com.paroquiaTeam.sgeParoquia.view.factory.PainelPrecificacaoPorHoraFactory;
 
@@ -18,9 +12,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.VBox;
 
-public class ConfiguracaoSistemaController {
-	private final Map<TipoPrecificacao, PainelPrecificacao> paineis = new EnumMap<>(TipoPrecificacao.class);
-	
+public class ConfiguracaoSistemaController {	
 	@FXML ChoiceBox<TipoPrecificacao> escolhaTipoPrecificacao;
 	@FXML VBox containerCamposPrecificacao;
 	
