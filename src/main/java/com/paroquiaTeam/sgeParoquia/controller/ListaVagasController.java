1b6ddcb6bed976c451031c97316d03a1cb6eb488
sebastiandevus <sebastiandevus@gmail.com>
2026-05-21 15:54:08 -0300

    Adiciona tela de vagas, com controller vazio

---
+++ src/main/java/com/paroquiaTeam/sgeParoquia/controller/ListaVagasController.java
@@ -0,0 +1,14 @@
+package com.paroquiaTeam.sgeParoquia.controller;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.SplitPane;
+
+public class ListaVagasController {
+	@FXML SplitPane splitPane; // painel principal da tela
+	
+	@FXML
+	private void initialize() {
+		
+	}
+}
