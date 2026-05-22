c42b3db9264393a13edaa7efe147612d335a1e4a
sebastiandevus <sebastiandevus@gmail.com>
2026-05-22 16:22:12 -0300

    Adiciona funcionalidade de ajuste de vagas à tela listaVagas

--- src/main/java/com/paroquiaTeam/sgeParoquia/controller/ListaVagasController.java
+++ src/main/java/com/paroquiaTeam/sgeParoquia/controller/ListaVagasController.java
@@ -1,14 +1,33 @@
 package com.paroquiaTeam.sgeParoquia.controller;
 
-import javafx.beans.property.SimpleDoubleProperty;
+import java.util.List;
+
+import com.paroquiaTeam.sgeParoquia.dao.VagaDAO;
+import com.paroquiaTeam.sgeParoquia.model.TipoReservaVaga;
+import com.paroquiaTeam.sgeParoquia.model.TipoVaga;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.SplitPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ListaVagasController {
-	@FXML SplitPane splitPane; // painel principal da tela
+	@FXML TextField vagasCarroComum;
+	@FXML TextField vagasMotoComum;
+	@FXML Button btnSalvar;
 	
 	@FXML
 	private void initialize() {
-		
+		btnSalvar.setOnAction(e -> {
+			//números de vaga, tipoReserva
+			long carroComum = Long.parseLong(vagasCarroComum.getText());
+			long motoComum = Long.parseLong(vagasMotoComum.getText());
+			
+			//Atualizando Vagas
+			VagaDAO dao = new VagaDAO();
+			dao.ajustarVagas(List.of(
+					new VagaDAO.AjusteVaga(TipoVaga.CARRO, TipoReservaVaga.COMUM, carroComum),
+					new VagaDAO.AjusteVaga(TipoVaga.MOTO, TipoReservaVaga.COMUM, motoComum)
+					));
+		});
 	}
 }
