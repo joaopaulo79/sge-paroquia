ad3369d3d4c293652b94576f63f3b4fe59b6823d
sebastiandevus <sebastiandevus@gmail.com>
2026-05-22 18:42:42 -0300

    Adiciona factory para cards de vaga

---
+++ src/main/java/com/paroquiaTeam/sgeParoquia/view/factory/VagaCardFactory.java
@@ -0,0 +1,40 @@
+package com.paroquiaTeam.sgeParoquia.view.factory;
+
+import com.paroquiaTeam.sgeParoquia.model.Vaga;
+import com.paroquiaTeam.sgeParoquia.view.components.CardGenerico;
+import com.paroquiaTeam.sgeParoquia.view.components.CardHeader;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class VagaCardFactory {
+	public CardGenerico criarCard(Vaga vaga) {
+		CardHeader header = new CardHeader(null);
+		
+		HBox conteudo = new HBox();
+		Label label = new Label();
+		conteudo.getChildren().add(label);
+		
+		javafx.scene.layout.VBox.setVgrow(conteudo, javafx.scene.layout.Priority.ALWAYS);
+		
+		if (vaga.isOcupada()) {
+			header.getStyleClass().add("card-header-atencao");
+			label.setText(vaga.getPlaca());
+		} else {
+			header.getStyleClass().add("card-header-primario");
+		}
+		
+		CardGenerico card = new CardGenerico(header, conteudo);
+		double largura = 150;
+		double altura = 80;
+		card.setMinWidth(largura);
+		card.setMinHeight(altura);
+		card.setPrefWidth(largura);
+		card.setPrefHeight(altura);
+		card.setMaxWidth(largura);
+		card.setMaxHeight(altura);
+		
+		card.getStyleClass().add("card");
+		return card;
+	}
+}
