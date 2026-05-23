8c1afb5d7c769c6ca17112fc33053b2ad6d02aa3
sebastiandevus <sebastiandevus@gmail.com>
2026-05-22 23:01:32 -0300

    Início de controller de usuário, comitando para não perder

---
+++ src/main/java/com/paroquiaTeam/sgeParoquia/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.paroquiaTeam.sgeParoquia.controller;
+
+import com.paroquiaTeam.sgeParoquia.model.Usuario;
+
+public class UsuarioController {
+	public void editar(Usuario usuario) {
+		System.out.println(usuario.toString());
+	}
+	
+	public void desabilitar(Usuario usuario) {
+		System.out.println(usuario.toString());
+	}
+}
