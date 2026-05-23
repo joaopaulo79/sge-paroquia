3879c9f35dd849de68ba0b626d0c65746ffb2a2b
sebastiandevus <sebastiandevus@gmail.com>
2026-05-22 23:00:13 -0300

    Card Factory de usuário. Não testado, commitando para não perder

---
+++ src/main/java/com/paroquiaTeam/sgeParoquia/view/factory/UsuarioCardFactory.java
@@ -0,0 +1,48 @@
+package com.paroquiaTeam.sgeParoquia.view.factory;
+
+import com.paroquiaTeam.sgeParoquia.controller.UsuarioController;
+import com.paroquiaTeam.sgeParoquia.model.Usuario;
+import com.paroquiaTeam.sgeParoquia.view.components.CardGenerico;
+import com.paroquiaTeam.sgeParoquia.view.components.CardHeader;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class UsuarioCardFactory {
+	private final UsuarioController controller;
+	
+	public UsuarioCardFactory(UsuarioController controller) {
+		this.controller = controller;
+	}
+	
+	public CardGenerico criarCardExpandivel(Usuario usuario) {
+		//Criando header
+		CardHeader header = new CardHeader(usuario.getId());
+		
+		if (usuario.isStatus()) {
+			header.getStyleClass().add("card-header-ativo");
+		} else {
+			header.getStyleClass().add("card-header-inativo");
+		}
+		
+		// Montando botões da linha principal
+		Button btnEditar = new Button("Editar");
+		Button btnDesativar = new Button("Desativar");
+		
+		btnEditar.setOnAction(e -> controller.editar(usuario));
+		btnDesativar.setOnAction(e -> controller.desabilitar(usuario));
+		
+		btnEditar.setAlignment(Pos.CENTER_RIGHT);
+		btnDesativar.setAlignment(Pos.CENTER_RIGHT);
+		
+		//Montando linha principal
+		HBox linhaPrincipal = new HBox();
+		linhaPrincipal.getChildren().add(new Label(usuario.getNome()));
+		linhaPrincipal.getChildren().add(new Label(usuario.getTipo().toString()));
+		linhaPrincipal.getChildren().addAll(btnEditar, btnDesativar);
+		
+		return new CardGenerico(header, linhaPrincipal);
+	}
+}
