7191760238e4346e239687a458e72289a27e4eb2
sebastiandevus <sebastiandevus@gmail.com>
2026-05-24 17:43:33 -0300

    Adiciona SessaoSistema aos testes

--- src/test/java/com/paroquiaTeam/sgeParoquia/dao/BaseDAOTest.java
+++ src/test/java/com/paroquiaTeam/sgeParoquia/dao/BaseDAOTest.java
@@ -2,11 +2,20 @@ package com.paroquiaTeam.sgeParoquia.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import com.paroquiaTeam.sgeParoquia.database.HibernateUtil;
+import com.paroquiaTeam.sgeParoquia.model.Caixa;
+import com.paroquiaTeam.sgeParoquia.model.TipoUsuario;
+import com.paroquiaTeam.sgeParoquia.model.Usuario;
+import com.paroquiaTeam.sgeParoquia.utils.SenhaUtil;
+import com.paroquiaTeam.sgeParoquia.utils.SessaoSistema;
 
 class BaseDAOTest {
 	protected static SessionFactory testSessionFactory;
@@ -15,10 +24,34 @@ class BaseDAOTest {
 	public static void init() {
 		testSessionFactory = HibernateUtil.buildSessionFactory("hibernate-test.cfg.xml");
 		HibernateUtil.setSessionFactory(testSessionFactory);
+		
+		String senha = SenhaUtil.hash("senha");
+		Usuario userLogadoTest = new Usuario("Logado", "logado", senha , true, TipoUsuario.ADMINISTRADOR);
+		Caixa caixaAtualTest = new Caixa(LocalDateTime.now(), null, 0, userLogadoTest);
+		
+		try (Session session = testSessionFactory.openSession()) {
+	        Transaction tx = session.beginTransaction();
+	        session.persist(userLogadoTest);
+	        session.persist(caixaAtualTest);
+	        tx.commit();
+	    }
+		
+		SessaoSistema sessao = SessaoSistema.getInstancia();
+		sessao.setUserLogado(userLogadoTest);
+		sessao.setCaixa(caixaAtualTest);
 	}
 	
 	@AfterAll
-	public static void cleanSf() {
+	public static void limpar() {
+		SessaoSistema sessaoSistema = SessaoSistema.getInstancia();
+		try (Session session = testSessionFactory.openSession()) {
+	        Transaction tx = session.beginTransaction();
+	        session.remove(sessaoSistema.getCaixa());
+	        session.remove(sessaoSistema.getUserLogado());
+	        tx.commit();
+	    }
+		
 		HibernateUtil.shutdown();
+		sessaoSistema.encerrarSessao();
 	}
 }
