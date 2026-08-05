c2f30d8039236a3e986a12282eb32efe10dc8816
sebastiandevus <sebastiandevus@gmail.com>
2026-08-05 20:12:17 -0300

    Adiciona DAO de convênio e testes para ele

---
+++ src/main/java/com/paroquiaTeam/sgeParoquia/dao/ConvenioDAO.java
@@ -0,0 +1,107 @@
+package com.paroquiaTeam.sgeParoquia.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.paroquiaTeam.sgeParoquia.database.HibernateUtil;
+import com.paroquiaTeam.sgeParoquia.model.Convenio;
+import com.paroquiaTeam.sgeParoquia.model.Usuario;
+import com.paroquiaTeam.sgeParoquia.utils.SenhaUtil;
+
+public class ConvenioDAO {
+	
+	public boolean exists(long id) { 
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT COUNT(c) FROM Convenio c WHERE c.id = ?1";
+			Long quantidade = sessao.createQuery(query, Long.class)
+					.setParameter(1, id)
+					.uniqueResult();			
+			return quantidade != null && quantidade > 0;
+		}
+	}
+	
+	public Optional<Convenio> getById(Long id) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT DISTINCT c FROM Convenio c WHERE c.id = :id";
+			return sessao.createQuery(query, Convenio.class)
+						.setParameter("id", id)
+						.uniqueResultOptional();
+					
+		}
+	}
+	
+	public Optional<Convenio> getByCnpj(Long id) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT DISTINCT c FROM Convenio c WHERE c.id = :id";
+			return sessao.createQuery(query, Convenio.class)
+						.setParameter("id", id)
+						.uniqueResultOptional();
+					
+		}
+	}
+	
+	public Optional<Convenio> getByNome(String nome) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT DISTINCT c FROM Convenio c WHERE c.nome = :nome";
+			return sessao.createQuery(query, Convenio.class)
+						.setParameter("nome", nome)
+						.uniqueResultOptional();
+					
+		}
+	}
+	
+	public List<Convenio> getAll() {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT c FROM Convenio c";
+			return sessao.createQuery(query, Convenio.class)
+						.getResultList();
+					
+		}
+	}
+	
+	public void save(Convenio convenio) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			Transaction t = sessao.beginTransaction();
+			try {
+				sessao.persist(convenio);
+				t.commit();
+			} catch (Exception e) {
+				t.rollback();
+				throw e;
+			}
+		}
+	}
+	
+	public void update(Convenio convenio) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			Transaction t = sessao.beginTransaction();
+			try {
+				sessao.merge(convenio);
+				t.commit();
+			} catch (Exception e) {
+				t.rollback();
+				throw e;
+			}
+		}
+	}
+	
+	public void delete(Long id) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			Transaction t = sessao.beginTransaction();
+			try {
+				Convenio convenio = sessao.get(Convenio.class, id);
+				if (convenio == null) {					
+					throw new IllegalArgumentException("Convênio com id especificado não encontrado");
+				}
+				sessao.remove(convenio);
+				t.commit();
+			} catch (Exception e) {
+				t.rollback();
+				throw e;
+			}
+		}
+	}
+}
---
+++ src/test/java/com/paroquiaTeam/sgeParoquia/dao/ConvenioDAOTest.java
@@ -0,0 +1,224 @@
+package com.paroquiaTeam.sgeParoquia.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.PropertyValueException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import com.paroquiaTeam.sgeParoquia.database.HibernateUtil;
+import com.paroquiaTeam.sgeParoquia.model.Convenio;
+import com.paroquiaTeam.sgeParoquia.model.StatusConvenio;
+
+class ConvenioDAOTest extends BaseDAOTest{
+	private static ConvenioDAO dao = new ConvenioDAO();
+	
+	@AfterEach
+	public void removeConvenios() {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+	        Transaction tx = session.beginTransaction();
+	        try {
+	            session.createMutationQuery("DELETE FROM Convenio c")
+	            .executeUpdate();
+	            tx.commit();
+	        } catch (Exception e) {
+	            tx.rollback();
+	            throw e;
+	        }
+	    }
+	}
+	
+	@Test
+	public void exists_retornaTrueComIdExistente() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		assertTrue(dao.exists((long) c.getId()));
+	}
+	
+	@Test
+	public void exists_retornaFalseComIdNaoExistente() {
+		assertFalse(dao.exists((long) 0));
+	}
+	
+	@Test
+	public void getById_retornaNadaComIdInvalido() {
+		Optional<Convenio> talvezConvenio = dao.getById((long) 1);
+		assertTrue(talvezConvenio.isEmpty());
+	}
+	
+	@Test
+	public void getById_retornaAlgoComIdValido() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		Optional<Convenio> talvezConvenio = dao.getById(c.getId());
+		assertTrue(talvezConvenio.isPresent());
+	}
+	
+	@Test
+	public void getByNome_retornaNadaComNomeInvalido() {
+		Optional<Convenio> talvezConvenio = dao.getByNome("nome");
+		assertTrue(talvezConvenio.isEmpty());
+	}
+	
+	@Test
+	public void getByNome_retornaAlgoComNomeValido() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		Optional<Convenio> talvezConvenio = dao.getByNome("Unimed");
+		assertTrue(talvezConvenio.isPresent());
+	}
+	
+	@Test
+	public void getAll_retornaMaisDeUmValorDoBanco() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		Convenio c2 = new Convenio("abc1234", "Unimed2", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c2);
+		
+		List<Convenio> usuarios = dao.getAll();
+		assertTrue(usuarios.size() > 1);
+	}
+	
+	@Test
+	public void save_persisteConvenioValido() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		Optional<Convenio> talvezConvenio = dao.getById(c.getId());
+		assertTrue(talvezConvenio.isPresent());
+	}
+	
+	@Test
+	public void save_cnpjDuplicadoThrowsConstraintViolationException() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		Convenio c2 = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		assertThrows(ConstraintViolationException.class, () -> dao.save(c2));
+	}
+	
+	@Test
+	public void save_cnpjNuloThrowsConstraintViolationException() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio(null, "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+
+		assertThrows(PropertyValueException.class, () -> dao.save(c));
+	}
+	
+	@Test
+	public void save_nomeNuloThrowsConstraintViolationException() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", null, 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+
+		assertThrows(PropertyValueException.class, () -> dao.save(c));
+	}
+	
+	@Test
+	public void save_statusNuloThrowsConstraintViolationException() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, null, 0.5, data);
+		
+		assertThrows(PropertyValueException.class, () -> dao.save(c));
+	}
+	
+	@Test
+	public void save_dataVencimentoNulaThrowsConstraintViolationException() {
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, null);
+
+		assertThrows(PropertyValueException.class, () -> dao.save(c));
+	}
+	
+	@Test
+	public void update_atualizaConvenioValido() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		c.setNome("Teste 2");
+		assertDoesNotThrow(() -> dao.update(c));
+	}
+	
+	@Test
+	public void update_cnpjDuplicadoThrowsConstraintViolationException() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		Convenio c2 = new Convenio("abc1234", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c2);
+		
+		c2.setCnpj("abc123");
+		assertThrows(ConstraintViolationException.class, () -> dao.update(c2));
+	}
+	
+	@Test
+	public void update_cnpjNuloThrowsConstraintViolationException() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		c.setCnpj(null);
+		assertThrows(PropertyValueException.class, () -> dao.update(c));
+	}
+	
+	@Test
+	public void update_nomeNuloThrowsConstraintViolationException() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		c.setNome(null);
+		assertThrows(PropertyValueException.class, () -> dao.update(c));
+	}
+
+	@Test
+	public void update_statusNuloThrowsConstraintViolationException() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		c.setStatus(null);
+		assertThrows(PropertyValueException.class, () -> dao.update(c));
+	}
+	
+	@Test
+	public void update_dataVencimentoNulaThrowsConstraintViolationException() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		c.setDataVencimento(null);
+		assertThrows(PropertyValueException.class, () -> dao.update(c));
+	}
+	
+	
+	@Test
+	public void delete_excluiUsuarioValido() {
+		LocalDate data = LocalDate.now().plusMonths(1);
+		Convenio c = new Convenio("abc123", "Unimed", 1.2, 10, StatusConvenio.ATIVO, 0.5, data);
+		dao.save(c);
+		
+		assertDoesNotThrow(() -> dao.delete(c.getId()));
+	}
+	
+	@Test
+	public void delete_idInvalidoThrows() {
+		assertThrows(IllegalArgumentException.class, () -> dao.delete((long) 1));
+	}
+}
