abb8b9375be26f083014fab0843d79f902fd5b32
sebastiandevus <sebastiandevus@gmail.com>
2026-08-07 00:51:39 -0300

    Primeiro commit do Service de Convenio
    Métodos para status ainda não estão implementados

---
+++ src/main/java/com/paroquiaTeam/sgeParoquia/service/ConvenioService.java
@@ -0,0 +1,94 @@
+package com.paroquiaTeam.sgeParoquia.service;
+
+import java.util.Optional;
+
+import com.paroquiaTeam.sgeParoquia.dao.ConvenioDAO;
+import com.paroquiaTeam.sgeParoquia.model.dto.DadosConvenio;
+import com.paroquiaTeam.sgeParoquia.model.entity.Convenio;
+
+public class ConvenioService {
+	
+	private ConvenioDAO dao = new ConvenioDAO();
+	
+	public Optional<Convenio> buscarPorCnpj(String cnpj) {
+		return dao.getByCnpj(cnpj);
+	}
+	
+	public Optional<Convenio> buscarPorNome(String nome) {
+		return dao.getByNome(nome);
+	}
+	
+	public void salvar (DadosConvenio dados) {
+		validarNovoConvenio(dados);
+		
+		Convenio convenio = dados.paraConvenio();
+		
+		dao.save(convenio);
+	}
+	
+	public void atualizar (DadosConvenio dadosNovos, Convenio convenio) {
+		validarAtualizacaoConvenio(dadosNovos, convenio);
+		
+		convenio.setCnpj(dadosNovos.cnpj());
+		convenio.setNome(dadosNovos.nome());
+		convenio.setMensalidade(dadosNovos.mensalidade());
+		convenio.setVagasCarro(dadosNovos.vagasCarro());
+		convenio.setVagasMoto(dadosNovos.vagasMoto());
+		convenio.setCobrancaIndividual(dadosNovos.cobrancaIndividual());
+		convenio.setDataVencimento(dadosNovos.dataVencimento());
+		
+		
+		
+		dao.update(convenio);
+	}
+	
+	public void desativar(Convenio convenio) {
+		
+	}
+	
+	private void validarNovoConvenio(DadosConvenio dados) {
+		validarCamposBasicos(dados);
+
+		if (dao.existsByCnpj(dados.cnpj())) {
+			throw new IllegalArgumentException("Convênio com este cnpj já existe");
+		}
+	}
+	
+	private void validarAtualizacaoConvenio(DadosConvenio dados, Convenio convenioAtual) {
+	    validarCamposBasicos(dados);
+
+	    boolean cnpjFoiAlterado = !dados.cnpj().equalsIgnoreCase(convenioAtual.getCnpj());
+	    
+	    if (cnpjFoiAlterado && dao.existsByCnpj(dados.cnpj())) {
+	        throw new IllegalArgumentException("O novo CNPJ informado já pertence a outro convênio.");
+	    }
+	}
+	
+	private void validarCamposBasicos(DadosConvenio dados) {
+		// Checando se o parametro em si passado não é nulo
+		if (dados == null) {
+	        throw new IllegalArgumentException("Os dados do convênio não podem ser nulos.");
+	    }
+
+	    if (dados.cnpj() == null || dados.cnpj().isBlank()) {
+	        throw new IllegalArgumentException("O CNPJ é obrigatório.");
+	    }
+
+	    if (dados.nome() == null || dados.nome().isBlank()) {
+	        throw new IllegalArgumentException("O nome do convênio é obrigatório.");
+	    }
+
+	    if (dados.status() == null) {
+	        throw new IllegalArgumentException("O status é obrigatório.");
+	    }
+
+	    if (dados.dataVencimento() == null) {
+	        throw new IllegalArgumentException("A data de vencimento é obrigatória.");
+	    }
+		
+		if (dados.mensalidade() < 0 || dados.vagasCarro() < 0 ||
+				dados.vagasMoto() < 0 || dados.cobrancaIndividual() < 0) {
+			throw new IllegalArgumentException("Dados numéricos não podem ser negativos");
+		}
+	}
+}
