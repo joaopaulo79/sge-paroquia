57c68b229e1c771f4a03ab23c4771ed7b0f5367a
sebastiandevus <sebastiandevus@gmail.com>
2026-08-15 08:35:27 -0300

    Adiciona DAO de veículo

---
+++ src/main/java/com/paroquiaTeam/sgeParoquia/dao/VeiculoDAO.java
@@ -0,0 +1,107 @@
+package com.paroquiaTeam.sgeParoquia.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.paroquiaTeam.sgeParoquia.database.HibernateUtil;
+import com.paroquiaTeam.sgeParoquia.model.entity.Cliente;
+import com.paroquiaTeam.sgeParoquia.model.entity.Veiculo;
+
+public class VeiculoDAO {
+	
+	public boolean existsById(long id) { 
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT COUNT(v) FROM Veiculo v WHERE v.id = ?1";
+			Long quantidade = sessao.createQuery(query, Long.class)
+					.setParameter(1, id)
+					.uniqueResult();			
+			return quantidade != null && quantidade > 0;
+		}
+	}
+	
+	public boolean existsByPlaca(String placa) { 
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT COUNT(v) FROM Veiculo v WHERE placa = ?1";
+			Long quantidade = sessao.createQuery(query, Long.class)
+					.setParameter(1, placa)
+					.uniqueResult();			
+			return quantidade != null && quantidade > 0;
+		}
+	}
+	
+	public Optional<Veiculo> getById(Long id) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT DISTINCT v FROM Veiculo v WHERE v.id = :id";
+			return sessao.createQuery(query, Veiculo.class)
+						.setParameter("id", id)
+						.uniqueResultOptional();
+					
+		}
+	}
+	
+	public Optional<Veiculo> getByCpf(String placa) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT DISTINCT v FROM Veiculo v WHERE v.placa = :placa";
+			return sessao.createQuery(query, Veiculo.class)
+						.setParameter("placa", placa)
+						.uniqueResultOptional();
+					
+		}
+	}
+	
+	
+	public List<Cliente> getAll() {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			String query = "SELECT v FROM Veiculo v";
+			return sessao.createQuery(query, Cliente.class)
+						.getResultList();
+					
+		}
+	}
+	
+	public void save(Veiculo veiculo) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			Transaction t = sessao.beginTransaction();
+			try {
+				sessao.persist(veiculo);
+				t.commit();
+			} catch (Exception e) {
+				t.rollback();
+				throw e;
+			}
+		}
+	}
+	
+	public void update(Veiculo veiculo) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			Transaction t = sessao.beginTransaction();
+			try {
+				sessao.merge(veiculo);
+				t.commit();
+			} catch (Exception e) {
+				t.rollback();
+				throw e;
+			}
+		}
+	}
+	
+	public void delete(Long id) {
+		try (Session sessao = HibernateUtil.getSessionFactory().openSession()){
+			Transaction t = sessao.beginTransaction();
+			try {
+				Veiculo veiculo = sessao.get(Veiculo.class, id);
+				if (veiculo == null) {					
+					throw new IllegalArgumentException("Veículo com id especificado não encontrado");
+				}
+				sessao.remove(veiculo);
+				t.commit();
+			} catch (Exception e) {
+				t.rollback();
+				throw e;
+			}
+		}
+	}
+}
