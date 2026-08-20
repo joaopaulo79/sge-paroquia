cc484c148f924c95417d8b07da40a593f00dcf34
sebastiandevus <sebastiandevus@gmail.com>
2026-08-20 19:41:45 -0300

    Tira import não usado

--- src/main/java/com/paroquiaTeam/sgeParoquia/dao/VeiculoDAO.java
+++ src/main/java/com/paroquiaTeam/sgeParoquia/dao/VeiculoDAO.java
@@ -7,7 +7,6 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.paroquiaTeam.sgeParoquia.database.HibernateUtil;
-import com.paroquiaTeam.sgeParoquia.model.entity.Cliente;
 import com.paroquiaTeam.sgeParoquia.model.entity.Veiculo;
 
 public class VeiculoDAO {
